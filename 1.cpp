1da728e86b519fb8c127347529edda2e6115bf48
MaxPan <inator@ukr.net>
2023-01-17 17:37:57 +0200

    Third commit

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+//git remote add origin {url}
+//git push -u origin master - add to github
+//git clone {url} - clone git progect
+//git pull origin master
 using namespace std;
 
 int main()
